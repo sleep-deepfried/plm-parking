943699fb02380290e633e7c945d4a2558121bd00
sleep-deepfried <pulidoearljohn14@gmail.com>
2023-12-23 02:59:00 +0800

    added footer and hero sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
+
 import Navbar from '../components/landing-navbar'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <div className='h-0.5 w-screen bg-gray-800'></div>
+      <Hero />
+      <Footer />
     </>
 
 
---
+++ src/components/hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero(){
+    return(
+        <>
+            <div className='bg-gray-900 h-96 w-screen'>
+                Hero
+            </div>
+        </>
+    );
+}
