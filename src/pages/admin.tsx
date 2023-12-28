e5c69f9644ed7e733279f1207bcc703be909975e
sleep-deepfried <pulidoearljohn14@gmail.com>
2023-12-28 23:04:50 +0800

    added user and admin page

---
+++ src/pages/admin.tsx
@@ -0,0 +1,9 @@
+export default function Admin(){
+    return(
+        <>
+            <div className='bg-gray-900 h-96 w-screen'>
+                Admin
+            </div>
+        </>
+    );
+}
---
+++ src/pages/user.tsx
@@ -0,0 +1,9 @@
+export default function User(){
+    return(
+        <>
+            <div className='bg-gray-900 h-96 w-screen'>
+                User
+            </div>
+        </>
+    );
+}
